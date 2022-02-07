d13d731e33a85953020fed2c6eed397a0661333b
PN <priteshrna@gmail.com>
2022-02-07 16:26:46 -0500

    initial file set up and commit

---
+++ YouNeedToExperience/YouNeedToExperienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouNeedToExperienceApp.swift
+//  YouNeedToExperience
+//
+//  Created by Pritesh Nadiadhara on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct YouNeedToExperienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
